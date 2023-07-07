2332c4e567ac354cfea7eba67387f8ec90a75739
rossjbm <118684575+rossjbm@users.noreply.github.com>
2023-07-07 09:18:29 -0400

    Imagenes, y funcion para boton de navegacion

---
+++ javascript/funcionalidad.js
@@ -0,0 +1,12 @@
+var id =document.getElementById("navegacion")
+function menuAbrir() {
+    id.style.display="flex";
+    id.style.flexDirection="column";
+    id.style.gap="25px";
+}
+function menuCerrar() {
+    if (id.style.display==="flex") {
+        id.style.display="none";
+        document.getElementById("boton-menu").style.display="block"
+    }
+}
